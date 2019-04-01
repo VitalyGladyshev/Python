5d3f9a9362691e0b067e821bf7832ab005c92e51
VitalyGladyshev <viv232@yandex.ru>
2019-04-01 18:04:30 +0300

    Ковыряю первую задачку

--- hw03_easy.py
+++ hw03_easy.py
@@ -29,7 +29,6 @@ print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
 
 print(my_round(100454454545.454545, 5))
-
 for ind in range(1, 17):
     print(my_round(454545454.545454545, ind))
 for ind in range(1, 16):
@@ -37,6 +36,43 @@ for ind in range(1, 16):
 print(f"float - это ЗЛО!!! При передаче в функцию ИСКАЖАЕТСЯ!!! Переделываем :(((")
 
 print("\nЗадание-1_2\n")
+
+
+def my_round_02(number, ndigits):
+    destination_list = list(str(number)) 
+    
+    # source_str = str(number)
+    # dot_pos = source_str.find('.')
+    # destination_str = ""
+    # if dot_pos < ndigits:
+    #     destination_str += source_str[:ndigits + 1]
+    # else:
+    #     destination_str += source_str[:ndigits]
+
+    
+    # for cnt in reversed(destination_list):
+        
+
+    # grad = dot_pos - ndigits
+    # if grad > 0:
+    #     destination_float = destination_float * 10 ** grad
+    # if ndigits >= dot_pos:
+    #     ndigits += 1
+    # if int(source_str[ndigits]) > 4:
+    #     destination_float += 10 ** grad
+
+    return destination_list
+
+
+print(my_round_02(2.1234567, 5))
+print(my_round_02(2.1999967, 5))
+print(my_round_02(2.9999967, 5))
+
+print(my_round_02(100454454545.454545, 5))
+for ind in range(1, 17):
+    print(my_round_02(454545454.545454545, ind))
+for ind in range(1, 16):
+    print(my_round_02(545454545.454545454, ind))
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
