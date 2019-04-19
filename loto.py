7e41ee1fff354a080d2067d18f383635e94a4750
VitalyGladyshev <viv232@yandex.ru>
2019-04-19 11:36:34 +0300

    Закончил loto

--- loto.py
+++ loto.py
@@ -53,98 +53,117 @@
 
 Подсказка: для работы с псевдослучайными числами удобно использовать 
 модуль random: http://docs.python.org/3/library/random.html
-
 """
+
 import os
 import random
 
+
 class Card:
-   def __init__(self):
-      self._barrel_count = 15
-      self._card_nubers_list = []
-      self._card_positions_list = []
-      for __ in range(0, 3):     # создаём карточку
-         self._card_positions_list += sorted(random.sample(list(range(0, 9)), 5))
-         while True:
-            numbers_row = random.sample(list(range(1, 91)), 5)
-            compare = False
-            for num in numbers_row:
-               if num in self._card_nubers_list:
-                  compare = True
-                  break
-            if compare:
-               continue
-            self._card_nubers_list += sorted(numbers_row)      
-            break
-      self._card_positions_list.append(-1)
-      
-   def print_card(self):      # печатаем карточку
-      index = 0
-      for __ in range(0, 3):
-         card_string = ''
-         for pos in range(0, 9):
-            if pos == self._card_positions_list[index]:
-               if self._card_nubers_list[index] > 9:
-                  card_string += str(self._card_nubers_list[index])
-               elif 0 <= self._card_nubers_list[index] <= 9:
-                  card_string += ' '
-                  card_string += str(self._card_nubers_list[index])
-               else:
-                  card_string += "--"
-               index += 1
-            else:
-               card_string += "  "
-            card_string += " "
-         print(card_string[:len(card_string) - 1])
+    def __init__(self):
+        self._field_remainder = 15
+        self._card_numbers_list = []
+        self._card_positions_list = []
+        for __ in range(0, 3):  # создаём карточку
+            self._card_positions_list += sorted(random.sample(list(range(0, 9)), 5))
+            while True:
+                numbers_row = random.sample(list(range(1, 91)), 5)
+                compare = False
+                for num in numbers_row:
+                    if num in self._card_numbers_list:
+                        compare = True
+                        break
+                if compare:
+                    continue
+                self._card_numbers_list += sorted(numbers_row)
+                break
+        self._card_positions_list.append(-1)
 
-   def new_barrel(self, burrel_number):      # сравниваем с числом на бочонке
-      try:
-         burrel_number = int(burrel_number)
-      except Exception:
-         return -1
-      if 1 > burrel_number > 91:
-         return -1
-      if burrel_number in self._card_nubers_list:
-         for ind in range(0, len(self._card_nubers_list)):
-            if burrel_number == self._card_nubers_list[ind]:
-               self._card_nubers_list[ind] = -1
-         return True
-      else:
-         return False
+    def print_card(self):  # печатаем карточку
+        index = 0
+        for __ in range(0, 3):
+            card_string = ''
+            for pos in range(0, 9):
+                if pos == self._card_positions_list[index]:
+                    if self._card_numbers_list[index] > 9:
+                        card_string += str(self._card_numbers_list[index])
+                    elif 0 <= self._card_numbers_list[index] <= 9:
+                        card_string += ' '
+                        card_string += str(self._card_numbers_list[index])
+                    else:
+                        card_string += "--"
+                    index += 1
+                else:
+                    card_string += "  "
+                card_string += " "
+            print(card_string[:len(card_string) - 1])
 
-   def get_barrel_count(self):      # выводим число оставшихся полей
-      return self._barrel_count
+    def new_barrel(self, barrel_number):  # сравниваем с числом на бочонке
+        try:
+            barrel_number = int(barrel_number)
+        except ValueError:
+            return -1
+        if 1 > barrel_number > 91:
+            return -1
+        if barrel_number in self._card_numbers_list:
+            for ind in range(0, len(self._card_numbers_list)):
+                if barrel_number == self._card_numbers_list[ind]:
+                    self._card_numbers_list[ind] = -1
+                    self._field_remainder -= 1
+            return 1
+        else:
+            return False
+
+    def get_field_remainder(self):  # выводим число оставшихся полей
+        return self._field_remainder
+
+
+def print_cards(barrel, barrel_rem):
+    os.system('cls')
+    print("Новый бочонок: {} (осталось {})".format(barrel, barrel_rem))
+    print("------ Ваша карточка -----")
+    player_card.print_card()
+    print("--------------------------")
+    print("-- Карточка компьютера ---")
+    computer_card.print_card()
+    print("--------------------------")
 
 
 player_card = Card()
 computer_card = Card()
-burrels_list = []
+barrels_list = []
 while True:
-   os.system('cls')
-   while True:
-      new_burrel = random.randint(1, 91)
-      if new_burrel in burrels_list:      
-         continue
-      else:
-         break
-   burrels_list.append(new_burrel)
-   print("Новый бочонок: {} (осталось {})".format(new_burrel, 90 - len(burrels_list)))
-   computer_card.new_barrel(new_burrel)
-   print("------ Ваша карточка -----")
-   player_card.print_card()
-   print("--------------------------")
-   print("-- Карточка компьютера ---")
-   computer_card.print_card()
-   print("--------------------------")
-   while True:
-      aswer = input("Зачеркнуть цифру? (y/n) (выйти q)")
-      if aswer == 'y':
-         player_card.new_barrel(new_burrel)
-         break
-      elif aswer == 'n':
-         player_card.new_barrel(new_burrel)
-      elif aswer == 'q':
-         exit()
-      else:
-         print("Некорректный ввод!")
-
+    while True:
+        new_barrel = random.randint(1, 90)
+        if new_barrel in barrels_list:
+            continue
+        else:
+            break
+    barrels_list.append(new_barrel)
+    computer_card.new_barrel(new_barrel)
+    print_cards(new_barrel, 90 - len(barrels_list))
+    while True:
+        answer = input("Зачеркнуть цифру? (y/n) (выйти q)")
+        if answer == 'y':
+            if not player_card.new_barrel(new_barrel):
+                print("Проигрыш! Такого номера нет в карточке!")
+                exit()
+            break
+        elif answer == 'n':
+            if player_card.new_barrel(new_barrel):
+                print("Проигрыш! Такой номер есть в карточке!")
+                exit()
+            break
+        elif answer == 'q':
+            exit()
+        else:
+            print("Некорректный ввод!")
+    if not computer_card.get_field_remainder() or not player_card.get_field_remainder():
+        print_cards(new_barrel, 90 - len(barrels_list))
+        if not computer_card.get_field_remainder() and not player_card.get_field_remainder():
+            print("Победила дружба! Поля в карточках закрыты одновременно!")
+        elif not player_card.get_field_remainder():
+            print("УРА!!! Вы победили!")
+        elif not computer_card.get_field_remainder():
+            print("Проигрыш! Компьютер победил :(")
+        break
